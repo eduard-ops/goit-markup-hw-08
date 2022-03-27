b4f7687be570e74cbbe6231e99c3cb57331a7c74
eduard-ops <epustynnik@gmail.com>
2022-03-28 00:22:43 +0300

    js

--- js/filter.js
+++ js/filter.js
@@ -8,9 +8,22 @@ function onClickBtn(e) {
   if (!e.target.classList.contains('filter__button')) return;
   let filterClass = e.target.dataset['f'];
   cards.forEach(cards => {
-    cards.classList.remove('anime');
     if (!cards.classList.contains(filterClass) && filterClass !== 'all') {
       cards.classList.add('anime');
+    } else {
+      cards.classList.remove('hide');
+      cards.classList.remove('anime');
     }
+  });
+  addTransition();
+}
+
+function addTransition() {
+  cards.forEach(card => {
+    card.ontransitionend = function () {
+      if (card.classList.contains('anime')) {
+        card.classList.add('hide');
+      }
+    };
   });
 }
